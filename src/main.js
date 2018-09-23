2646a6dcd727c9c90d31858873580ff72d36b195
Henri Bergius <henri.bergius@iki.fi>
2018-09-23 18:20:09 +0200

    Provide a CLI interface

--- src/main.js
+++ src/main.js
@@ -1,22 +1,45 @@
+const program = require('commander');
+const manifest = require('../package.json');
 const websocket = require('./websocket');
 const server = require('./server');
 const puppeteer = require('./puppeteer');
 
-const port = 3001;
+exports.run = (options) => {
+  const opts = options;
+  if (!opts.root) {
+    opts.root = process.cwd();
+  }
+  if (!opts.port) {
+    opts.port = 3569;
+  }
+  return server(opts.root, opts.file)(opts.port)
+    .then(httpServer => puppeteer(`http://localhost:${opts.port}`)
+      .then(pageProxy => websocket(httpServer)
+        .then(wsProxy => [pageProxy, wsProxy])))
+    .then(([pageProxy, wsProxy]) => {
+      wsProxy.on('message', msg => pageProxy.send(msg));
+      pageProxy.on('message', msg => wsProxy.send(msg));
+      return true;
+    });
+};
 
-process.on('SIGTERM', () => {
-  process.exit(0);
-});
+exports.main = () => {
+  program
+    .version(manifest.version)
+    .option('-r, --root <dir>', 'Base dir to expose for the browser')
+    .option('-f, --file <file>', 'Path to the browser build of NoFlo inside base dir')
+    .option('-p --port <port>', 'Runtime port to expose')
+    .parse(process.argv);
 
-server(process.cwd(), 'spec/dist/noflo.js')(port)
-  .then(httpServer => puppeteer(`http://localhost:${port}`)
-    .then(pageProxy => websocket(httpServer)
-      .then(wsProxy => [pageProxy, wsProxy])))
-  .then(([pageProxy, wsProxy]) => {
-    wsProxy.on('message', msg => pageProxy.send(msg));
-    pageProxy.on('message', msg => wsProxy.send(msg));
-    console.log('Running!');
-  })
-  .catch((err) => {
-    console.log(err);
+  process.on('SIGTERM', () => {
+    process.exit(0);
   });
+
+  exports.run(program)
+    .then(() => {
+      console.log('Running!');
+    }, (err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
